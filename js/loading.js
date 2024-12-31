d3661c83c8073074a43b73ba3fe8a0fc7e550322
constanthing <thingconstant@gmail.com>
2024-12-30 19:51:07 -0500

    testing

--- js/loading.js
+++ js/loading.js
@@ -1,48 +1,76 @@
-const loading = document.querySelector("#loading");
-loading.addEventListener("animationend", ()=>{
-    document.querySelector("main").classList.remove("hidden")
-})
+window.addEventListener("load", () => {
+    // default files: loading.js, loading.css -> loaded
 
-const loadingFilter = loading.querySelector("#loading-filter");
-loadingFilter.addEventListener("animationend", () => {
-    loading.classList.add("animate-disappear")
-})
+    console.log("loaded")
+    const styles = ["index", "gallery", "filter", "export", "cursor", "media"];
+    for (const style of styles) {
+        // dynamically adding styles to load site faster 
+        const link = document.createElement("link");
+        link.rel = "stylesheet";
+        link.href = `./css/${style}.css`;
+        document.querySelector("head").appendChild(link)
 
-function getRandomNumber(max) {
-    return parseInt(Math.random() * max) + 1;
-}
-function getRandomColor() {
-    return [getRandomNumber(255), getRandomNumber(255), getRandomNumber(255)];
-}
-async function randomBorder() {
-    let index = 0;
-    while (index < 10) {
-        let r = getRandomColor()
-        loadingFilter.style.borderTopColor = `rgb(${r[0]}, ${r[1]}, ${r[2]})`;
-
-        r = getRandomColor();
-        loadingFilter.style.borderRightColor = `rgb(${r[0]}, ${r[1]}, ${r[2]})`;
-
-        r = getRandomColor();
-        loadingFilter.style.borderBottomColor = `rgb(${r[0]}, ${r[1]}, ${r[2]})`;
-
-        r = getRandomColor();
-        loadingFilter.style.borderLeftColor = `rgb(${r[0]}, ${r[1]}, ${r[2]})`;
-        await new Promise((resolve, reject) => {
-            setTimeout(() => resolve(), 100)
-        })
-        index += 1;
+        //  fires when stylesheet downloaded and applied
+        link.onload = ()=>{
+            console.log(`${style}.css finished downloading`)
+        }
     }
-    return new Promise((resolve, reject) => {
-        resolve()
+
+    const imgs = document.querySelectorAll("#gallery img");
+    for (const img of imgs) {
+        img.onload = ()=> {
+            console.log(img + " loaded")
+        };
+    }
+    console.log(imgs)
+
+
+    const loading = document.querySelector("#loading");
+    loading.addEventListener("animationend", () => {
+        document.querySelector("main").classList.remove("hidden")
     })
-}
 
-async function animate() {
-    await randomBorder()
+    const loadingFilter = loading.querySelector("#loading-filter");
+    loadingFilter.addEventListener("animationend", () => {
+        loading.classList.add("animate-disappear")
+    })
 
-    loadingFilter.classList.add("animate-borderr");
+    function getRandomNumber(max) {
+        return parseInt(Math.random() * max) + 1;
+    }
+    function getRandomColor() {
+        return [getRandomNumber(255), getRandomNumber(255), getRandomNumber(255)];
+    }
+    async function randomBorder() {
+        let index = 0;
+        while (index < 10) {
+            let r = getRandomColor()
+            loadingFilter.style.borderTopColor = `rgb(${r[0]}, ${r[1]}, ${r[2]})`;
 
-    // shouldn't this go to complete white as borders finish ?
-}
-animate()
+            r = getRandomColor();
+            loadingFilter.style.borderRightColor = `rgb(${r[0]}, ${r[1]}, ${r[2]})`;
+
+            r = getRandomColor();
+            loadingFilter.style.borderBottomColor = `rgb(${r[0]}, ${r[1]}, ${r[2]})`;
+
+            r = getRandomColor();
+            loadingFilter.style.borderLeftColor = `rgb(${r[0]}, ${r[1]}, ${r[2]})`;
+            await new Promise((resolve, reject) => {
+                setTimeout(() => resolve(), 100)
+            })
+            index += 1;
+        }
+        return new Promise((resolve, reject) => {
+            resolve()
+        })
+    }
+
+    async function animate() {
+        await randomBorder()
+
+        loadingFilter.classList.add("animate-borderr");
+
+        // shouldn't this go to complete white as borders finish ?
+    }
+    animate()
+})
