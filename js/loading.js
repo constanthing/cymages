c19c5d2f4994f5f7f2362b4c28333789dbe5a70c
constanthing <thingconstant@gmail.com>
2024-12-30 20:27:13 -0500

    filter border error

--- js/loading.js
+++ js/loading.js
@@ -1,4 +1,47 @@
 window.addEventListener("load", async () => {
+
+    const loading = document.querySelector("#loading");
+    loading.addEventListener("animationend", () => {
+        document.querySelector("main").classList.remove("hidden")
+    })
+
+    const loadingFilter = loading.querySelector("#loading-filter");
+    loadingFilter.addEventListener("animationend", () => {
+        loading.classList.add("animate-disappear")
+    })
+
+    function getRandomNumber(max) {
+        return parseInt(Math.random() * max) + 1;
+    }
+    function getRandomColor() {
+        return [getRandomNumber(255), getRandomNumber(255), getRandomNumber(255)];
+    }
+    async function randomBorder() {
+        let index = 0;
+        while (index < 10) {
+            let r = getRandomColor()
+            loadingFilter.style.borderTopColor = `rgb(${r[0]}, ${r[1]}, ${r[2]})`;
+
+            r = getRandomColor();
+            loadingFilter.style.borderRightColor = `rgb(${r[0]}, ${r[1]}, ${r[2]})`;
+
+            r = getRandomColor();
+            loadingFilter.style.borderBottomColor = `rgb(${r[0]}, ${r[1]}, ${r[2]})`;
+
+            r = getRandomColor();
+            loadingFilter.style.borderLeftColor = `rgb(${r[0]}, ${r[1]}, ${r[2]})`;
+            await new Promise((resolve, reject) => {
+                setTimeout(() => resolve(), 100)
+            })
+            index += 1;
+        }
+        return new Promise((resolve, reject) => {
+            resolve()
+        })
+    }
+
+    randomBorder()
+
     // default files: loading.js, loading.css -> loaded
 
     async function loadFiles(files, domManipulation) {
@@ -53,6 +96,9 @@ window.addEventListener("load", async () => {
     console.log('11111-------')
     console.log("SCRIPTS LOADED")
 
+    // hide the loadingFilter reveal the CONTENT
+    loadingFilter.classList.add("animate-borderr");
+
     const imgs = document.querySelectorAll("#gallery img");
     for (const img of imgs) {
         img.onload = () => {
@@ -62,52 +108,4 @@ window.addEventListener("load", async () => {
     console.log(imgs)
 
 
-    const loading = document.querySelector("#loading");
-    loading.addEventListener("animationend", () => {
-        document.querySelector("main").classList.remove("hidden")
-    })
-
-    const loadingFilter = loading.querySelector("#loading-filter");
-    loadingFilter.addEventListener("animationend", () => {
-        loading.classList.add("animate-disappear")
-    })
-
-    function getRandomNumber(max) {
-        return parseInt(Math.random() * max) + 1;
-    }
-    function getRandomColor() {
-        return [getRandomNumber(255), getRandomNumber(255), getRandomNumber(255)];
-    }
-    async function randomBorder() {
-        let index = 0;
-        while (index < 10) {
-            let r = getRandomColor()
-            loadingFilter.style.borderTopColor = `rgb(${r[0]}, ${r[1]}, ${r[2]})`;
-
-            r = getRandomColor();
-            loadingFilter.style.borderRightColor = `rgb(${r[0]}, ${r[1]}, ${r[2]})`;
-
-            r = getRandomColor();
-            loadingFilter.style.borderBottomColor = `rgb(${r[0]}, ${r[1]}, ${r[2]})`;
-
-            r = getRandomColor();
-            loadingFilter.style.borderLeftColor = `rgb(${r[0]}, ${r[1]}, ${r[2]})`;
-            await new Promise((resolve, reject) => {
-                setTimeout(() => resolve(), 100)
-            })
-            index += 1;
-        }
-        return new Promise((resolve, reject) => {
-            resolve()
-        })
-    }
-
-    async function animate() {
-        await randomBorder()
-
-        loadingFilter.classList.add("animate-borderr");
-
-        // shouldn't this go to complete white as borders finish ?
-    }
-    animate()
 })
